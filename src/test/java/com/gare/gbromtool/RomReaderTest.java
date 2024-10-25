b3dddb268cc1516a4e13546ef5c4de4dd89f60f4
TankedThomas <TankedThomas@users.noreply.github.com>
2024-10-26 01:57:06 +1300

    Add unit test skeletons for later use

--- src/test/java/com/gare/gbromtool/RomReaderTest.java
+++ src/test/java/com/gare/gbromtool/RomReaderTest.java
@@ -4,12 +4,12 @@
  */
 package com.gare.gbromtool;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+//import org.junit.jupiter.api.AfterEach;
+//import org.junit.jupiter.api.AfterAll;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.BeforeAll;
+//import org.junit.jupiter.api.Test;
+//import static org.junit.jupiter.api.Assertions.*;
 
 /**
  *
@@ -20,20 +20,20 @@ public class RomReaderTest {
     public RomReaderTest() {
     }
     
-    @BeforeAll
-    public static void setUpClass() {
-    }
-    
-    @AfterAll
-    public static void tearDownClass() {
-    }
-    
-    @BeforeEach
-    public void setUp() {
-    }
-    
-    @AfterEach
-    public void tearDown() {
-    }
+//    @BeforeAll
+//    public static void setUpClass() {
+//    }
+//    
+//    @AfterAll
+//    public static void tearDownClass() {
+//    }
+//    
+//    @BeforeEach
+//    public void setUp() {
+//    }
+//    
+//    @AfterEach
+//    public void tearDown() {
+//    }
     
 }
