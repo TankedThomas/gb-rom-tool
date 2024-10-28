9d573dc78cc47740017e1902cd2568aa572962a6
TankedThomas <TankedThomas@users.noreply.github.com>
2024-10-29 12:46:19 +1300

    Use RomSpec for logo constant

--- src/main/java/com/gare/gbromtool/RomReader.java
+++ src/main/java/com/gare/gbromtool/RomReader.java
@@ -37,14 +37,7 @@ public class RomReader {
         byte[] logo = Arrays.copyOfRange(romData, 0x104, 0x134);
 
         // The Logo expected in the ROM header to check against
-        byte[] checkLogo = {
-            (byte) 0xCE, (byte) 0xED, (byte) 0x66, (byte) 0x66, (byte) 0xCC, (byte) 0x0D, (byte) 0x00, (byte) 0x0B,
-            (byte) 0x03, (byte) 0x73, (byte) 0x00, (byte) 0x83, (byte) 0x00, (byte) 0x0C, (byte) 0x00, (byte) 0x0D,
-            (byte) 0x00, (byte) 0x08, (byte) 0x11, (byte) 0x1F, (byte) 0x88, (byte) 0x89, (byte) 0x00, (byte) 0x0E,
-            (byte) 0xDC, (byte) 0xCC, (byte) 0x6E, (byte) 0xE6, (byte) 0xDD, (byte) 0xDD, (byte) 0xD9, (byte) 0x99,
-            (byte) 0xBB, (byte) 0xBB, (byte) 0x67, (byte) 0x63, (byte) 0x6E, (byte) 0x0E, (byte) 0xEC, (byte) 0xCC,
-            (byte) 0xDD, (byte) 0xDC, (byte) 0x99, (byte) 0x9F, (byte) 0xBB, (byte) 0xB9, (byte) 0x33, (byte) 0x3E
-        };
+        byte[] checkLogo = RomSpec.BOOT_LOGO;
 
         System.out.println("Extracted logo bytes: " + HexFormat.of().formatHex(logo));
         System.out.println("Expected logo bytes: " + HexFormat.of().formatHex(checkLogo));
